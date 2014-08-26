400536ed1bfb62cef641158c3cda53107d7c2497
yangtingting <489677274@qq.com>
2014-08-27 00:39:47 +0800

    add start frame

--- gen/com/yagami/wwcdc/R.java
+++ gen/com/yagami/wwcdc/R.java
@@ -28,7 +28,8 @@ public final class R {
         public static final int start3=0x7f020004;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080001;
+        public static final int mFrame=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
--- src/com/yagami/wwcdc/MainActivity.java
+++ src/com/yagami/wwcdc/MainActivity.java
@@ -1,22 +1,64 @@
 package com.yagami.wwcdc;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.os.SystemClock;
 import android.app.Activity;
+import android.graphics.drawable.Drawable;
 import android.view.Menu;
+import android.widget.FrameLayout;
 
 public class MainActivity extends Activity {
+	FrameLayout frame=null;
+	private boolean flag=true;
+	
+	class MyHandler extends Handler{
+		int i=0;
+		public void handleMessage(Message msg) {
+			
+			show(i);
+			i++;
+			sleep(1000);
+			
+		}
+		public void sleep(long delayMillis) {
+			if (flag) {
+				sendMessageDelayed(obtainMessage(0),delayMillis);
+			}
+		}
+	}
+	void show(int j){
+		Drawable a=getResources().getDrawable(R.drawable.icon);
+		Drawable b=getResources().getDrawable(R.drawable.start2);
+		Drawable c=getResources().getDrawable(R.drawable.start3);
+		switch (j) {
+		case 0:
+			frame.setForeground(a);
+			break;
+		case 1:
+			frame.setForeground(b);
+			break;
+		case 2:
+			frame.setForeground(c);
+			break;
+		default:
+			break;
+		}
+
+	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-	}
+		
+		frame=(FrameLayout)findViewById(R.id.mFrame);
+		
+		final MyHandler myHandler=new MyHandler();
+		myHandler.sleep(100);
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+		
 	}
 
 }
