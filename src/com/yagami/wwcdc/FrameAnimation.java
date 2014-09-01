ca769f0090bbeb3a398594fb98d7727f80c54e92
yangtingting <489677274@qq.com>
2014-09-02 00:11:07 +0800

    use FrameAnimation

---
+++ src/com/yagami/wwcdc/FrameAnimation.java
@@ -0,0 +1,61 @@
+package com.yagami.wwcdc;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+
+public class FrameAnimation extends Activity {
+	private FrameView startView;
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		
+		startView=new FrameView(this);
+		setContentView(startView);
+	}
+	public boolean onKeyDown(int keyCode,KeyEvent event){
+		if (startView==null) {
+			return false;
+		}
+		if (keyCode==KeyEvent.KEYCODE_BACK) {
+			this.finish();
+			return true;
+		}
+		return super.onKeyDown(keyCode, event);
+		
+		
+	}
+	public class FrameView extends View{
+		private AnimationDrawable frameAnimation=null;
+		Context mContext=null;
+
+		public FrameView(Context context) {
+			super(context);
+			mContext=context;
+			
+			ImageView img=new ImageView(mContext);
+			img.setBackgroundResource(R.anim.frame);
+			frameAnimation=(AnimationDrawable)img.getBackground();
+			this.setBackground(frameAnimation);
+		}
+		public boolean onTouchEvent(MotionEvent event) {
+			if (event.getPointerCount()==1) {
+				frameAnimation.start();
+			}else if (event.getPointerCount()==2) {
+				frameAnimation.stop();
+			}
+			return true;
+			
+			
+		}
+		
+	}
+	
+
+}
